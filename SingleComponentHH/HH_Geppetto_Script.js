fcc0f4a1765893474f5bfa0dc68ff5b259f4cd57
Padraig Gleeson <p.gleeson@gmail.com>
2016-09-12 16:49:02 +0100

    Tweaks

--- SingleComponentHH/HH_Geppetto_Script.js
+++ SingleComponentHH/HH_Geppetto_Script.js
@@ -22,10 +22,10 @@ Plot2.setLegend(hhcell.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q,"Sodi
 Plot2.setLegend(hhcell.hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q,"Potassium n.q");
 
 G.addWidget(Widgets.POPUP);
-Popup1.setMessage("The Hodgkin-Huxley model is a mathematical model that describes how action potentials in neurons are initiated and propagated. It is a set of nonlinear differential equations that approximates the electrical characteristics of excitable cells such as neurons. <br/><br/> The <b>original OSB project</b> for this model is <a target='_blank' href='http://www.opensourcebrain.org/projects/hodgkin-huxley-tutorial'>here</a> and what you are viewing now is a <b>prerecorded simulation</b> of the model, which was run in the NEURON simulator.<br/><br/>You can run your own simulations of this model by signing up to OSB, signing in, going to the <a target='_blank' href='http://www.opensourcebrain.org/projects/hodgkin-huxley-tutorial'>OSB project</a> and clicking on <b>Explore model</b>.");
+Popup1.setMessage("The <b>Hodgkin-Huxley model</b> is a mathematical model that describes how action potentials in neurons are initiated and propagated. It is a set of nonlinear differential equations that approximates the electrical characteristics of excitable cells such as neurons. <br/><br/> The <b>original OSB project</b> for this model is <a target='_blank' href='http://www.opensourcebrain.org/projects/hodgkin-huxley-tutorial'>here</a> and what you are viewing now is a <b>prerecorded simulation</b> of the model, which was run in the NEURON simulator.<br/><br/>You can run your own simulations of this model by signing up to OSB, signing in, going to the <a target='_blank' href='http://www.opensourcebrain.org/projects/hodgkin-huxley-tutorial'>OSB project</a> and clicking on <b>Explore model</b>.<br/><br/>There is also <a target='_blank' href='http://hodgkin-huxley-tutorial.readthedocs.io/en/latest/'>a tutorial for the HH model</a>, which has been developed as part of the <a target='_blank' href='http://www.openworm.org/'>OpenWorm project</a>.");
 Popup1.setName("Description");
-Popup1.setPosition(1174,142)
-Popup1.setSize(319.8,433.8)
+Popup1.setPosition(1074,142)
+Popup1.setSize(391.8,454.8)
 
 G.addBrightnessFunction(hhcell.hhpop[0], hhcell.hhpop[0].v, function(x){return (x+0.07)/0.1;});
 
