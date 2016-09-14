d3f03e0e4e5859ff42252b66c1b6dfb0c251e6ae
Padraig Gleeson <p.gleeson@gmail.com>
2016-09-14 12:12:41 +0100

    Update desc

--- CA1/script.js
+++ CA1/script.js
@@ -2,9 +2,9 @@
 
 Model.neuroml.morphology_CA1.kad_dendrite_group.show(true)
 
-G.addWidget(1).setMessage(Project.getActiveExperiment().getDescription());
+G.addWidget(1).setMessage("Hippocampus CA1 pyramidal cell from M. Migliore, M. Ferrante, GA Ascoli (2005). The model shows how the back- and forward propagation of action potentials in the oblique dendrites of CA1 neurons could be modulated by local properties such as morphology or active conductances.<br/><br/> The <b>original OSB project</b> for this model is <a target='_blank' href='http://www.opensourcebrain.org/projects/ca1pyramidalcell'>here</a>.");
 Popup1.setPosition(950,98);
-Popup1.setSize(138,500);
+Popup1.setSize(260.8,474.8)
 Popup1.setName("Description");
 
 //Toolbar
