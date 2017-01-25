3ce6596d6f8f21b340a1489a52885d5705f19fe8
Padraig Gleeson <p.gleeson@gmail.com>
2017-01-25 17:45:15 +0000

    Changes from master in move_scripts branch

--- AuditoryCortex/script.js
+++ AuditoryCortex/script.js
@@ -1,4 +1,4 @@
-Model.neuroml.resolveAllImportTypes();
+Model.neuroml.resolveAllImportTypes(function() { $(".osb-notification").hide(); });
 
 Project.getActiveExperiment().playAll();
 
@@ -8,7 +8,7 @@ Connectivity1.setData(acnet2,{linkType:function(c){return GEPPETTO.ModelFactory.
 
 G.addWidget(1);
 //Popup1.setMessage(Project.getActiveExperiment().getDescription());
-Popup1.setMessage("Network model of primary auditory cortex in rodents by David Beeman, University of Colorado, Boulder.<br/><br/> The <b>original OSB project</b> for this model is <a target='_blank' href='http://opensourcebrain.org/projects/acnet2'>here</a> and what you are viewing now is a <b>prerecorded simulation</b> of the model, which was run in the NEURON simulator.<br/><br/>You can run your own simulations of this model by signing up to OSB, signing in, going to the <a target='_blank' href='http://opensourcebrain.org/projects/acnet2'>OSB project</a> and clicking on <b>Explore model</b>.")
+Popup1.setMessage("Network model of primary auditory cortex in rodents by David Beeman, University of Colorado, Boulder.<br/><br/> The <b>original OSB project</b> for this model can be found <a target='_blank' href='http://opensourcebrain.org/projects/acnet2'>here</a> and what you are viewing now is a <b>prerecorded simulation</b> of the model, which was run in the NEURON simulator.<br/><br/>You can run your own simulations of this model by signing up to OSB, signing in, going to the <a target='_blank' href='http://opensourcebrain.org/projects/acnet2'>OSB project</a> and clicking on <b>Explore model</b>.")
 Popup1.setName("Description");
 G.incrementCameraPan(-0.1, 0)
 Connectivity1.setPosition(1042,256)
@@ -17,7 +17,7 @@ Popup1.setSize(256.8,441.8)
 acnet2.baskets_12[0].select()
 
 G.addWidget(0);
-Plot1.setName("Primary Auditory Cortext Network - Some membrane potentials");
+Plot1.setName("Primary Auditory Cortex Network - Some membrane potentials");
 Plot1.setPosition(113, 90);
 Plot1.setSize(230,445);
 Plot1.plotData(acnet2.pyramidals_48[0].soma_0.v);
