3ed717d03bdfaba7fa738163d09ba8daa8c31262
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-06-02 10:47:00 +0100

    Update script2.js

--- AuditoryCortex/script2.js
+++ AuditoryCortex/script2.js
@@ -10,7 +10,6 @@ G.addWidget(1);
 //Popup1.setMessage(Project.getActiveExperiment().getDescription());
 Popup1.setMessage("Network model of primary auditory cortex in rodents by David Beeman, University of Colorado, Boulder.<br/><br/> The <b>original OSB project</b> for this model is <a target='_blank' href='http://opensourcebrain.org/projects/acnet2'>here</a> and what you are viewing now is a <b>prerecorded simulation</b> of the model, which was run in the NEURON simulator.<br/><br/>You can run your own simulations of this model by signing up to OSB, signing in, going to the <a target='_blank' href='http://opensourcebrain.org/projects/acnet2'>OSB project</a> and clicking on <b>Explore model</b>.")
 Popup1.setName("Description");
-G.incrementCameraPan(-0.1, 0)
 Connectivity1.setPosition(1042,256)
 Popup1.setPosition(116,318)
 Popup1.setSize(256.8,441.8)
@@ -23,7 +22,6 @@ Plot1.setSize(230,445);
 Plot1.plotData(acnet2.pyramidals_48[0].soma_0.v);
 Plot1.plotData(acnet2.pyramidals_48[0].apical0_1.v);
 
-G.addBrightnessFunctionBulkSimplified([acnet2.pyramidals_48[0].soma_0.v, acnet2.pyramidals_48[0].apical0_1.v, acnet2.pyramidals_48[0].apical2_2.v, acnet2.pyramidals_48[0].apical3_3.v, acnet2.pyramidals_48[0].apical4_4.v, acnet2.pyramidals_48[0].apical1_5.v, acnet2.pyramidals_48[0].basal0_6.v, acnet2.pyramidals_48[0].basal1_7.v, acnet2.pyramidals_48[0].basal2_8.v], function(x){return (x+0.07)/0.1;});
 
 initOSBGeppetto("network","acnet2")
 
